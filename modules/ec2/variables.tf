dcfe371e18dc2e8fcb4f23b1fd41489749d80019
FZ5851 <lcaminale@octo.com>
2020-01-20 09:39:50 +0100

    add ec2 modules

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "The aws region the resources will be build"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "ami_id" {
+  description = "id of an ami by default it's ubuntu 16.04"
+  type        = string
+  default     = "ami-03ef731cc103c9f09"
+}
+
+variable "instance_type" {
+  description = "aws ec2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tag_name" {
+  description = "Aws tag name permit to search an instance by tag"
+  type        = string
+}
+
+variable "aws_public_key_ssh" {
+  description = "The key name of the Key Pair to use for the instance"
+  type        = string
+}
