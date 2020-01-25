9d28b4f8c90dc5797c0ae22207439bab900ea14f
FZ5851 <lcaminale@octo.com>
2020-01-25 18:11:29 +0100

    update outputs

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -12,3 +12,8 @@ output "ssh_connect_cli" {
   description = "The command to enter for ssh connect to the machine"
   value       = "ssh -i ~/.ssh/id_rsa_aws ec2-user@${aws_instance.my-ec2.public_dns}"
 }
+
+output "arn" {
+  description = "Arn of the EC2 instance"
+  value = aws_instance.my-ec2.arn
+}
